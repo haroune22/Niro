f59bb0bbc68ef4eaf4f5d56600b6d19df68f4773
haroune22 <harounehamenniche123@gmail.com>
2024-05-09 19:09:56 +0100

    card actions

--- components/modals/rename-modal.tsx
+++ components/modals/rename-modal.tsx
@@ -11,17 +11,43 @@ import {
   DialogTitle,
   DialogTrigger
 } from "../ui/dialog"
+import { FormEventHandler, useEffect, useState } from "react"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+import { useApiMutation } from "@/hooks/use-api-mutation"
+import { api } from "@/convex/_generated/api"
+import { toast } from "sonner"
 
 
 export const RenameModal = () => {
 
+  
   const {
     initialValues,
     isOpen,
     onClose,
     onOpen
   } = useRenameModal();
+  
+  const { mutate, pending} = useApiMutation(api.board.update)
+  const [title,setTitle] = useState(initialValues.title);
 
+  useEffect(() => {
+    setTitle(initialValues.title)
+  }, [initialValues.title]);
+
+  const onSubmit :FormEventHandler <HTMLFormElement>= (e) => {
+    e.preventDefault()
+    mutate({
+      id:initialValues.id,
+      title
+    })
+    .then(() => {
+      toast.success("Board renamed")
+      onClose()
+    })
+    .catch(()=> toast.error("Failed to rename board"))
+  }
 
   return (
 
@@ -35,6 +61,26 @@ export const RenameModal = () => {
         <DialogDescription>
           Enter an new title for this board
         </DialogDescription>
+        <form onSubmit={onSubmit} className="space-y-4">
+          <Input 
+            disabled={pending}
+            required
+            maxLength={60}
+            value={title}
+            onChange={(e)=> setTitle(e.target.value)}
+            placeholder="Board title"
+           />
+           <DialogFooter>
+            <DialogClose>
+              <Button type="button" variant="outline" >
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button disabled={pending} type="submit" >
+                Save
+            </Button>
+           </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   )
