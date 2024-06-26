df25aa68ca71483a668936d474cf189b68b6efbe
haroune22 <harounehamenniche123@gmail.com>
2024-06-26 18:41:21 +0100

    canvas layout

---
+++ app/board/[boardId]/_components/Tolbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Tolbar = () => {
+  return (
+    <div className='absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 '>
+        <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
+            <div className="">Pencil</div>
+            <div className="">cirlce</div>
+            <div className="">Pencil</div>
+        </div>
+        <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
+            <div className="">Undo</div>
+            <div className="">Redo</div>
+        </div>
+    </div>
+  )
+}
---
+++ app/board/[boardId]/_components/canvas.tsx
@@ -0,0 +1,22 @@
+"use client"
+import React from 'react'
+import { Info } from './Info'
+import { Participants } from './Participants'
+import { Tolbar } from './Tolbar'
+
+interface CanvasProps {
+  boardId:string
+}
+export const Canvas = ({
+  boardId
+}:CanvasProps) => {
+  return (
+    <main 
+        className='h-full w-full relative bg-neutral-100 touch-none'
+    >
+        <Info/>
+        <Participants/>
+        <Tolbar/>
+    </main>
+  )
+}
