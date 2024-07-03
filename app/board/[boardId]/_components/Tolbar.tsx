6e0de6b459eee0536f1bcf0d57c0b10ba44ee435
haroune22 <harounehamenniche123@gmail.com>
2024-07-03 11:27:08 +0100

    toolbar Butoons

--- app/board/[boardId]/_components/Tolbar.tsx
+++ app/board/[boardId]/_components/Tolbar.tsx
@@ -1,16 +1,64 @@
 "use client"
 
+import { Circle, MousePointer2, Pencil, Redo2, Square, StickyNote, Type, Undo, Undo2 } from "lucide-react"
+import { ToolButton } from "./ToolButton"
+
 export const Tolbar = () => {
   return (
     <div className='absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 '>
         <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-            <div className="">Pencil</div>
-            <div className="">cirlce</div>
-            <div className="">Pencil</div>
+          <ToolButton
+            label="Select"
+            icon={MousePointer2}
+            onClick={()=>{}}
+            isActive={false}
+           />
+          <ToolButton
+            label="Text"
+            icon={Type}
+            onClick={()=>{}}
+            isActive={false}
+           />
+          <ToolButton
+            label="Sticky note"
+            icon={StickyNote}
+            onClick={()=>{}}
+            isActive={false}
+           />
+          <ToolButton
+            label="Rectangle"
+            icon={Square}
+            onClick={()=>{}}
+            isActive={false}
+           />
+          <ToolButton
+            label="Elipse"
+            icon={Circle}
+            onClick={()=>{}}
+            isActive={false}
+           />
+          <ToolButton
+            label="Pen"
+            icon={Pencil}
+            onClick={()=>{}}
+            isActive={false}
+           />
         </div>
         <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-            <div className="">Undo</div>
-            <div className="">Redo</div>
+          <ToolButton
+            label="Undo"
+            icon={Undo2}
+            onClick={()=>{}}
+            isActive={false}
+            isDisabled={true}
+          />
+          <ToolButton
+            label="Redo"
+            icon={Redo2}
+            onClick={()=>{}}
+            isActive={false}
+            isDisabled={true}
+          />
         </div>
     </div>
   )
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        board: "hover:bg-blue-500/20",
+        board: "hover:bg-blue-500/20 hover:text-blue-8000",
         boardActive: "bg-blue-500/20 text-blue-800"
       },
       size: {
